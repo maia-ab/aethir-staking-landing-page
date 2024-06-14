b887398ae6921f19c752628115a9c2c3265e1cee
unknown <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-06-13 21:06:39 -0300

    added header and footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] });
 
@@ -17,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className} bg-[#DBF48C]`}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+import Logo from "./logo";
+
+export default function Header() {
+  return (
+    <div>
+      <Logo />
+      <h1>Header</h1>
+    </div>
+  );
+}
